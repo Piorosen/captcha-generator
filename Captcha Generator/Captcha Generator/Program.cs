1594acf590e3022cd48a778f5e956d3488f8fe9a
Piorosen <aoikazto@gmail.com>
2019-08-21 03:51:06 +0900

    package update and gen project

---
+++ Captcha Generator/Captcha Generator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+
+namespace Captcha_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
