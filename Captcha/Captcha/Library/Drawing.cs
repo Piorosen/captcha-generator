001a1576be221423534edd0cd7d3f21e6c08f276
aoikazto@naver.com <aoikazto@gmail.com>
2019-08-21 22:12:32 +0900

    Random Font Size

--- Captcha/Captcha/Library/Drawing.cs
+++ Captcha/Captcha/Library/Drawing.cs
@@ -9,21 +9,34 @@ namespace Captcha.Library
 {
     public class Drawing
     {
+        PointF GetPosition(int num, int size)
+        {
+            return new PointF
+            {
+                X = Option.Instance.Random.Next(0, Option.Instance.ImageSize.Width / size) + (Option.Instance.ImageSize.Width / size * num),
+                Y = Option.Instance.Random.Next(0, Option.Instance.ImageSize.Height)
+            };
+        }
+
         public bool Draw(string savePath, string value)
         {
-            Bitmap image = new Bitmap(200, 40);
+            Bitmap image = new Bitmap(Option.Instance.ImageSize.Width
+                                        , Option.Instance.ImageSize.Height);
             
             Graphics g = Graphics.FromImage(image);
             g.FillRectangle(Brushes.White, 0, 0, image.Width, image.Height);
 
-            g.DrawString(value, SystemFonts.DefaultFont, Brushes.Black, 20, 10);
+            for (int i = 0; i < value.Length; i++)
+            {
+                var pos = GetPosition(i, value.Length);
+                var font = new Font(SystemFonts.DefaultFont.FontFamily, 20.0F);
+
+                g.DrawString(value[i].ToString(), font, Brushes.Black, pos);
+            }
 
             image.Save(savePath);
 
             return true;
         }
-
-
-
     }
 }
--- Captcha/Captcha/Library/Option.cs
+++ Captcha/Captcha/Library/Option.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+
 namespace Captcha.Library
 {
     public class Option : Singleton<Option>
@@ -23,6 +25,16 @@ namespace Captcha.Library
         public int Epoch = 4;
         public readonly Random Random = new Random();
 
+        public Size RangeFontSize = new Size(10, 20);
+        public float FontSize
+        {
+            get
+            {
+                return (float)(Random.NextDouble() * (RangeFontSize.Height - RangeFontSize.Width) + RangeFontSize.Width);
+            }
+        }
+
+        public Size ImageSize = new Size(200, 40);
 
     }
 }
